cb833c8a9f228fd37d2b210a60e15edd60e65b15
Sham Tze Wah <sham@rbtsb.com>
2023-10-18 21:13:43 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+//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
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    private String readFile(){
+        StringBuilder strBuilder = new StringBuilder();
+        //1. Read File
+        //2. Extract all the fields
+        //3.
+        return strBuilder.toString();
+    }
+}
